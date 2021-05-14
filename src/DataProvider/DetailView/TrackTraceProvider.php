61bbbdb917f0cb0949c4834286c4e115822a37cd
Bram Gerritsen <bgerritsen@emico.nl>
2021-05-14 10:48:19 +0200

    Add track trace information to order endpoints

---
+++ src/DataProvider/DetailView/TrackTraceProvider.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * @author Bram Gerritsen <bgerritsen@emico.nl>
+ * @copyright (c) Emico B.V. 2017
+ */
+
+namespace Emico\RobinHq\DataProvider\DetailView;
+
+use Emico\RobinHq\DataProvider\Helper\Tracking;
+use Emico\RobinHqLib\Model\Order\DetailsView;
+use Magento\Sales\Api\Data\OrderInterface;
+
+class TrackTraceProvider implements DetailViewProviderInterface
+{
+    /**
+     * @var Tracking
+     */
+    private $tracking;
+
+    /**
+     * TrackTraceProvider constructor.
+     * @param Tracking $tracking
+     */
+    public function __construct(Tracking $tracking)
+    {
+        $this->tracking = $tracking;
+    }
+
+    /**
+     * @param OrderInterface $order
+     * @return array
+     * @throws \Exception
+     */
+    public function getItems(OrderInterface $order): array
+    {
+        $trackingData = $this->tracking->getTrackingData($order);
+        if (empty($trackingData)) {
+            return [];
+        }
+
+        $detailView = new DetailsView(DetailsView::DISPLAY_MODE_DETAILS, $trackingData);
+        $detailView->setCaption(__('track & trace'));
+        return [$detailView];
+    }
+}
