bc79111b76a979c18d93278fb8cba432fec92959
Pieter Zandbergen <p.zandbergen@emico.nl>
2021-07-20 14:39:42 +0200

    Track and Trace details

---
+++ src/DataProvider/Helper/Tracking.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * @author Emico B.V.
+ * @copyright (c) Emico B.V. 2021
+ */
+
+namespace Emico\RobinHq\DataProvider\Helper;
+
+use Magento\Framework\Escaper;
+use Magento\Sales\Api\Data\OrderInterface;
+use Magento\Sales\Api\Data\TrackInterface;
+use Magento\Shipping\Model\Carrier\AbstractCarrierOnline;
+use Magento\Shipping\Model\CarrierFactory;
+use Magento\Shipping\Model\ResourceModel\Order\Track\Collection;
+use Magento\Shipping\Model\Tracking\Result\Status;
+
+class Tracking
+{
+    /**
+     * @var CarrierFactory
+     */
+    private $carrierFactory;
+
+    /**
+     * @var Escaper
+     */
+    private $escaper;
+
+    /**
+     * Tracking constructor.
+     * @param CarrierFactory $carrierFactory
+     */
+    public function __construct(CarrierFactory $carrierFactory, Escaper $escaper)
+    {
+        $this->carrierFactory = $carrierFactory;
+        $this->escaper = $escaper;
+    }
+
+    /**
+     * Retrieve all tracking URL's
+     *
+     * @param OrderInterface $order
+     * @return array
+     */
+    public function getTrackingData(OrderInterface $order): array
+    {
+        $trackingData = [];
+
+        /** @var Collection $tracksCollection */
+        $tracksCollection = $order->getTracksCollection();
+
+        /** @var TrackInterface $track */
+        foreach ($tracksCollection->getItems() as $track) {
+            $url = $this->getTrackingUrl($track);
+            $trackData = array_filter(array_merge([
+                'title' => $track->getTitle(),
+                'track_number' => $track->getTrackNumber(),
+            ], $url ? [
+                '' => '<div class="panel_button"><div><a href="'
+                    . $this->escaper->escapeHtmlAttr($url)
+                    . '" target="_blank">Details</a></div></div>'
+            ] : []));
+            if (empty($trackData)) {
+                continue;
+            }
+            $trackingData[] = $trackData;
+        }
+
+        return $trackingData;
+    }
+
+    /**
+     * @param TrackInterface $track
+     * @return string|null
+     */
+    protected function getTrackingUrl(TrackInterface $track)
+    {
+        $carrierInstance = $this->carrierFactory->create($track->getCarrierCode());
+        if (!$carrierInstance || !($carrierInstance instanceof AbstractCarrierOnline)) {
+            return null;
+        }
+
+        $trackingInfo = $carrierInstance->getTrackingInfo($track->getTrackNumber());
+        if (!$trackingInfo) {
+            return null;
+        }
+
+        if ($trackingInfo instanceof Status && $trackingInfo->getData('url') !== null) {
+            return (string) $trackingInfo->getData('url');
+        }
+
+        return null;
+    }
+}
